518eac0fbfb469372c0ed41ca9fd99f4ace72b66
walla2j9 <walla2j9@ucmail.uc.edu>
2026-06-14 19:26:38 -0400

    Complete Part 1

---
+++ src/Part_1.java
@@ -0,0 +1,18 @@
+public class Part_1 {
+    void main(){
+        int numberOfKids = 2;
+        boolean isRaining = false;
+        double priceOfGas = 3.99;
+        int favNumber = 7;
+        double shoeSize = 8.5;
+        int birthMonth = 9;
+        String fullName = "Jillian Wallace";
+        System.out.println("I have " + numberOfKids + " kids.");
+        System.out.println("The statement 'it is raining' is " + isRaining + ".");
+        System.out.println("The price of a gallon of gas is $" + priceOfGas + ".");
+        System.out.println("My favorite number is " + favNumber + ".");
+        System.out.println("My shoe size is " + shoeSize + ".");
+        System.out.println("My birth month is " + birthMonth + ".");
+        System.out.println("My full name is " + fullName + ".");
+    }
+}
